793dc44203b67f6fb3cdf3bde62c9620b9be1e3c
Philipp Hagemeister <phihag@phihag.de>
2019-05-24 05:30:14 +0200

    exit when assertion fails

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 
 (async () => {
+try {
 
 const browser = await require('puppeteer').launch({args: ['--no-sandbox']});
 const page = await browser.newPage();
 await page.setContent('_\u2714_');
 console.log(await page.content());
-//require('assert')((await page.content()).includes('_\u2714_'));
+require('assert')((await page.content()).includes('_\u2714_'));
 
+} catch(e) {
+console.error(e.stack);
+process.exit(2);
+}
 })();
